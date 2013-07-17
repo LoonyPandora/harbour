3e46c9e0a880768dd690020238faf95fd4f0fcd9
James Aitken <loonypandora@gmail.com>
2013-07-17 02:42:54 +0100

    add a pie chart, for fun

--- mockups/html/v3/js/main.js
+++ mockups/html/v3/js/main.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
 
     var grid = new hashgrid();
 
+
+    // Example Chart
+    $(".canvas-pie-chart").easyPieChart({
+      animate: 500,
+      scaleColor: false,
+      lineWidth: 5,
+      // rotate: -90,
+      lineCap: "round",
+      size: 100,
+      trackColor: "#bdc3c7",
+      barColor: "#2980b9"
+    });
+
 });

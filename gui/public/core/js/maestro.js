31d3881571e722e7f8737b78fe12b927f1c4020a
James Aitken <loonypandora@gmail.com>
2013-07-26 22:29:21 +0100

    Update backbone boilerplate

--- gui/public/core/js/maestro.js
+++ gui/public/core/js/maestro.js
@@ -24,6 +24,7 @@
             return Maestro.Module._privateList;
         },
 
+        // Registers a module, and all it's routes
         register: function (name) {
             // If this module has already been created, return it.
             if (Maestro.Module._privateList[name]) {
@@ -66,23 +67,30 @@
     
         // Trigger the initial route and enable HTML5 History API support
         Backbone.history.start({
-            pushState: true
+            pushState: true,
+            root: "/"
         });
     
         // All navigation that is relative should be passed through the navigate
         // method, to be processed by the router.  If the link has a data-bypass
         // attribute, bypass the delegation completely.
-        $(document).on("click", "a:not([data-bypass])", function (evt) {
-            // Get the anchor href and protcol
-            var href = $(this).attr("href");
-            var protocol = this.protocol + "//";
-    
-            // Ensure the protocol is not part of URL, meaning its relative.
-            if (href && href.slice(0, protocol.length) !== protocol) {
+        $(document).on("click", "a[href]:not([data-bypass])", function(evt) {
+            // Get the absolute anchor href.
+            var $this = $(this);
+            var href = { prop: $this.prop("href"), attr: $this.attr("href") };
+
+            // Get the absolute root.
+            var root = location.protocol + "//" + location.host + "/";
+
+            // Ensure the root is part of the anchor href, meaning it's relative.
+            if (href.prop.slice(0, root.length) === root) {
                 // Stop the default event to ensure the link will not cause a page refresh.
                 evt.preventDefault();
 
-                app.router.navigate(href, true);
+                // `Backbone.history.navigate` is sufficient for all Routers and will
+                // trigger the correct events. The Router's internal `navigate` method
+                // calls this anyways.  The fragment is sliced from the root.
+                Backbone.history.navigate(href.attr, true);
             }
         });
     });

21a9d5bf214edd565c0b0fb94d7ee6c26c6985ea
James Aitken <loonypandora@gmail.com>
2013-07-26 20:57:56 +0100

    Add a loader that actually works

--- gui/public/core/js/backbone.maestro.js
+++ gui/public/core/js/backbone.maestro.js
@@ -1,13 +1,42 @@
 (function () {
     "use strict";
 
-    Backbone.MaestroView = Backbone.View.extend({
-    });
+    // The top-level namespace. All public Maestro classes and modules will be attached to this.
+    this.Maestro = {};
 
-    Backbone.MaestroModel = Backbone.Model.extend({
-    });
+    // Extend the default Backbone Router / View / Model / Collection
+    Maestro.Router = Backbone.Router.extend({ });
 
-    Backbone.MaestroCollection = Backbone.Collection.extend({
-    });
+    Maestro.View = Backbone.View.extend({ });
 
-})();
+    Maestro.Model = Backbone.Model.extend({ });
+
+    Maestro.Collection = Backbone.Collection.extend({ });
+
+    // Our own Module getters & settings
+    Maestro.Module = {
+        // If no name specified, or it doesn't exist - return all modules
+        get: function (name) {
+            if (Maestro.Module._privateList[name]) {
+                return Maestro.Module._privateList[name];
+            }
+
+            return Maestro.Module._privateList;
+        },
+
+        register: function (name) {
+            // If this module has already been created, return it.
+            if (Maestro.Module._privateList[name]) {
+                return Maestro.Module._privateList[name];
+            }
+
+            // If not, create a blank object and return that
+            Maestro.Module._privateList[name] = {};
+            return Maestro.Module._privateList[name];
+        },
+
+        // FIXME: Close over this, but keep a getter for all modules
+        _privateList: {}
+    }
+
+}).call(this);

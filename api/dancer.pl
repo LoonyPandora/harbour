c8ae8f78fb7b63db591e1eecb1847ac383dbb702
James Aitken <loonypandora@gmail.com>
2013-12-05 14:03:32 +0000

    Cleanup module paths

--- api/dancer.pl
+++ api/dancer.pl
@@ -6,12 +6,12 @@ use common::sense;
 use Path::Tiny qw(path);
 
 # Get a list of all installed modules and load them one-by-one
-my $moduledir = path(dirname(__FILE__), "../modules/HarbourX");
+my $moduledir = path(dirname(__FILE__), "../modules");
 
 for my $module ($moduledir->children) {
-    if ( $module->is_dir && $module->child("lib", $module->basename . ".pm")->is_file ) {
-        push @INC, $module->child("lib")->stringify;
-        load_app($module->basename);
+    if ( $module->is_dir && $module->child("lib", "HarbourX")->is_dir ) {
+        push @INC, $module->child("lib")->realpath->stringify;
+        load_app("HarbourX::".$module->basename);
     }
 }
 

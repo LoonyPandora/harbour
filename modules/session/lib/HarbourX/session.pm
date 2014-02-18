d1e11d45a01564e2293f6c6b9a42d25addfaaf2d
James Aitken <loonypandora@gmail.com>
2014-02-18 18:38:05 +0000

    Add fixes for the header links

--- modules/session/lib/HarbourX/session.pm
+++ modules/session/lib/HarbourX/session.pm
@@ -8,10 +8,11 @@ my @core_js = (
     "/core/js/pace.js",
     "/core/js/jquery.js",           "/core/js/bootstrap.js",
     "/core/js/underscore.js",       "/core/js/backbone.js",
+    "/core/js/harbour.config.js",
     "/core/js/hashgrid.js",         "/core/js/highlight.js",
     "/core/js/backbone.cacheit.js", "/core/js/markdown.js",
     "/core/js/moment.js",           "/core/js/harbour.js",
-    "/core/js/harbour.config.js",   "/core/js/spin.js",
+    "/core/js/spin.js",
     "/core/js/jquery.spin.js",      "/core/js/jquery.transit.js",
     "/core/js/main.js",
 );

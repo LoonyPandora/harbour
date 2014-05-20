cf1da52f162100165c1970a8cc94ba7faa1ab431
James Aitken <loonypandora@gmail.com>
2014-05-21 00:15:24 +0100

    Attempt to load the module required for authorising that route

--- api/dancer.pl
+++ api/dancer.pl
@@ -4,6 +4,10 @@ use Dancer;
 use Harbour;
 use common::sense;
 use Path::Tiny qw(path);
+use Module::Load qw();
+use JSON::XS;
+use File::Slurp;
+use Try::Tiny;
 
 # Get a list of all installed modules and load them one-by-one
 my $moduledir = path(dirname(__FILE__), "../modules");
@@ -12,6 +16,22 @@ for my $module ($moduledir->children) {
     if ( $module->is_dir && $module->child("lib", "HarbourX")->is_dir ) {
         push @INC, $module->child("lib")->realpath->stringify;
         load_app("HarbourX::".$module->basename);
+    }
+
+    if ( $module->is_dir && $module->child("config", "authorisation.json")->is_file ) {
+        my $auth = decode_json(
+            read_file($module->child("config", "authorisation.json")->realpath->stringify)
+        );
+
+        for my $auth_module (@{$auth->{authorisation}}) {
+            my $module = $auth_module->{type} =~ s/[^A-Za-z:]//rg;
+
+            try {
+                Module::Load::load($module)
+            } catch {
+                warn "Couldn't load Authorisation module for route: " . $auth_module->{route} . " Module: $module";
+            };
+        }
     }
 }
 

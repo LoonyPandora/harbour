875f24ae0e252e95dbd5c8d6bba6819e54405274
James Aitken <loonypandora@gmail.com>
2014-05-20 22:09:12 +0100

    Add a before hook stub

--- api/lib/Harbour.pm
+++ api/lib/Harbour.pm
@@ -1,7 +1,13 @@
 package Harbour;
 
 use Dancer ':syntax';
+use Data::Dump qw(dump);
 use common::sense;
+
+hook 'before' => sub {
+    # die dump Dancer::App->current;
+    # request->path_info('/foo/oversee')
+};
 
 1;
 

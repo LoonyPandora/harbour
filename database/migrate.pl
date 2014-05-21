9a177348c85e0477a57097ae1dff0c38aaa8132a
James Aitken <loonypandora@gmail.com>
2014-05-21 22:23:31 +0100

    Add a schema file so we can create a DB from nothing

--- database/migrate.pl
+++ database/migrate.pl
@@ -8,6 +8,7 @@ use Harbour::Database::Migrator;
 my $asdf = Harbour::Database::Migrator->new_with_options({
     migrations_dir   => "./migrations",
     migration_table  => "applied_migration",
+    schema_file      => "./schema.sql",
 });
 
 $asdf->create_or_update_database();

c3b982cbd1cd93a0e89cbba80a7fa044e9dbe09f
James Aitken <loonypandora@gmail.com>
2014-05-21 02:20:34 +0100

    Load auth modules dynamically

--- api/dancer.pl
+++ api/dancer.pl
@@ -4,13 +4,13 @@ use Dancer;
 use Harbour;
 use common::sense;
 use Path::Tiny qw(path);
-use Module::Load qw();
 use JSON::XS;
-use File::Slurp;
-use Try::Tiny;
 
 # Get a list of all installed modules and load them one-by-one
 my $moduledir = path(dirname(__FILE__), "../modules");
+
+# Get a structure for all routes
+my $route_authorisation = {};
 
 for my $module ($moduledir->children) {
     if ( $module->is_dir && $module->child("lib", "HarbourX")->is_dir ) {
@@ -18,25 +18,21 @@ for my $module ($moduledir->children) {
         load_app("HarbourX::".$module->basename);
     }
 
+    # Load the authorisation file and later we'll make it a setting run on a before filter
     if ( $module->is_dir && $module->child("config", "authorisation.json")->is_file ) {
         my $auth = decode_json(
-            read_file($module->child("config", "authorisation.json")->realpath->stringify)
+            $module->child("config", "authorisation.json")->slurp_utf8
         );
 
-        for my $auth_module (@{$auth->{authorisation}}) {
-            my $module = $auth_module->{type} =~ s/[^A-Za-z:]//rg;
-
-            try {
-                Module::Load::load($module)
-            } catch {
-                warn "Couldn't load Authorisation module for route: " . $auth_module->{route} . " Module: $module";
-            };
-        }
+        $route_authorisation->{$module->basename} = $auth;
     }
 }
 
 # Tell Dancer about where we store our config
 set "confdir" => path(dirname(__FILE__), "config")->stringify;
 set "envdir"  => path(dirname(__FILE__), "config/environments")->stringify;
+
+set "route_authorisation" => $route_authorisation;
+
 
 dance();
--- api/lib/Harbour.pm
+++ api/lib/Harbour.pm
@@ -2,11 +2,62 @@ package Harbour;
 
 use Dancer ':syntax';
 use Data::Dump qw(dump);
+use Try::Tiny;
+use Module::Load qw();
+
 use common::sense;
 
+
+# Returns the class for checking whether a user is authorised
+# to access this route with the given arguments
+sub is_authorised {
+    my ($route, $harbour_module) = @_;
+
+     # route is a regex, so munge it
+    if ($route =~ /\Q?^ux:\E/) {
+        # Strip the perl regexp flags
+        $route =~ s/\Q?^ux:\E//g;
+        # Strip negative lookaheads
+        $route =~ s/ \( \? ! [^)]+ \) //gx;
+        # Change named captures to :params
+        $route =~ s/ \( \? < ([\w]+) >.*? \) /:$1/gx;
+        # Anything left is nonsense that can be ignored.
+        $route =~ s/[^\/:\w]//g;
+    }
+
+    # This setting is dynamically generated on app startup by reading all modules
+    my $route_authorisation = setting("route_authorisation");
+
+    # Find the relevent entry in the file, and we'll try to load it first
+    my $auth_package = $route_authorisation->{$harbour_module}->{$route};
+
+    # Without an entry, default to deny.
+    unless ($auth_package) {
+        status 401;
+        return halt "Sorry, I can't do that.";
+    }
+
+    # If we have an entry, try to load it and run it.
+    try {
+        Module::Load::load($auth_package);
+    } catch {
+        die "Couldn't load authorisation module for Route: $route Harbour Module: $harbour_module Perl Module: $auth_package";
+    };
+
+    # Now lets try and run the authorised method
+    unless ($auth_package->authorised()) {
+        status 401;
+        return halt "Sorry, I can't do that.";
+    }
+}
+
+
+# This runs the authorisation method for every route, loading the module if necessary
 hook 'before' => sub {
-    # die dump Dancer::App->current;
-    # request->path_info('/foo/oversee')
+    my $route = shift;
+    my ($module) = request()->path =~ m{^/(\w+)/};
+
+    is_authorised($route->pattern(), $module);
 };
 
 1;

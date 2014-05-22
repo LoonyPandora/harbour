a7c9babfd403e7202dc51ac9f260e3ba65c94c9f
James Aitken <loonypandora@gmail.com>
2014-05-22 03:48:13 +0100

    cleanup

--- modules/webui/public/js/webui.mixin.js
+++ modules/webui/public/js/webui.mixin.js
@@ -2,9 +2,7 @@
     "use strict";
 
     WebUI.Mixin = {
-        url: "/",
-        name: "Harbour",
-        // icon: "anchor" // Disable this, it won't show in the sidebar
+
     };
 
 })(Harbour.Module.register("webui"));

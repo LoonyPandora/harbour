3b9a94508bbe7b493a4a7a87d34c542b10035b15
James Aitken <loonypandora@gmail.com>
2014-05-22 03:29:36 +0100

    Update session table for dancer sessions

--- database/migrations/2014-05-22-002539-add-session-table/add-session-table.sql
+++ database/migrations/2014-05-22-002539-add-session-table/add-session-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `session` (
-  `id` char(36) NOT NULL,
+  `id` varchar(36) NOT NULL,
   `session_data` TEXT NOT NULL,
   `last_active` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)

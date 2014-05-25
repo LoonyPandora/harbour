f5456aa5f70eea2f64006ba5710c30bd571ab263
James Aitken <loonypandora@gmail.com>
2014-05-25 16:22:57 +0100

    Add a fixutres table for mocked data

--- database/migrations/2014-05-22-001154-add-user-table/add-user-table.sql
+++ database/migrations/2014-05-22-001154-add-user-table/add-user-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` char(36) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `password` varchar(128) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`),
   KEY `uuid` (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

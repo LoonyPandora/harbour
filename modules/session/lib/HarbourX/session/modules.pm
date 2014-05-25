45e1f62ba089096fb4c252bd3d29b529a0efa663
James Aitken <loonypandora@gmail.com>
2014-05-25 18:06:16 +0100

    Add a route to list all routes.
    Eventually will have a documentation list

--- modules/session/lib/HarbourX/session/modules.pm
+++ modules/session/lib/HarbourX/session/modules.pm
@@ -1,6 +1,8 @@
 package HarbourX::session::modules;
 
 use Dancer ":syntax";
+use Data::Dump qw(dump);
+
 use common::sense;
 
 
@@ -16,6 +18,40 @@ sub enabled_modules {
 get "/session/modules" => sub {
     return enabled_modules();
 };
+
+# Test route with a regex
+get qr{^ /session/regex_route/ (?<repo_owner> .+ ) / (?<repo_name> .+ ) $}x => sub {
+
+};
+
+
+# Lists all routes the currently authenticated user has access to
+# Of course they may not be authorised to access the resource at that route - but they can try
+get "/session/route_list" => sub {
+    my @apps = Dancer::App->applications;
+    
+    my $route_list = {};
+    for my $app ( @apps ) {
+        my $routes = $app->{registry}->{routes};
+
+        # TODO: could probably do some fancy map here.
+        for my $method (keys %$routes) {
+            for my $route (@{ $routes->{$method} }) {
+                my $route_key = Harbour::regex_to_route($route->pattern);
+
+                # Only add authorised routes
+                if (Harbour::is_authorised($route->pattern, $app->{name})) {
+                    $route_list->{$route_key}->{route} = Harbour::regex_to_route($route->pattern);
+                    $route_list->{$route_key}->{role} = Harbour::get_authorisation_package($route->pattern, $app->{name});
+                    push @{$route_list->{$route_key}->{methods}}, $method;
+                }
+            }
+        }
+    }
+
+    return $route_list;
+};
+
 
 
 get "/session/current" => sub {

4bb4902d89cfddd3f9fc5fdaa6070d65642aab92
James Aitken <loonypandora@gmail.com>
2014-05-25 16:22:40 +0100

    Remove useless named variable

--- database/migrate.pl
+++ database/migrate.pl
@@ -5,12 +5,12 @@ use common::sense;
  
 use Harbour::Database::Migrator;
 
-my $asdf = Harbour::Database::Migrator->new_with_options({
+my $migrations = Harbour::Database::Migrator->new_with_options({
     migrations_dir   => "./migrations",
     migration_table  => "applied_migration",
     schema_file      => "./schema.sql",
 });
 
-$asdf->create_or_update_database();
+$migrations->create_or_update_database();
 
 1;

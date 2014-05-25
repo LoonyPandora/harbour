79fbf8884ba4150d353d9616f5c260ad21ec6af1
James Aitken <loonypandora@gmail.com>
2014-05-25 19:58:48 +0100

    Return something suitable for backbone.js

--- modules/documentation/lib/HarbourX/documentation.pm
+++ modules/documentation/lib/HarbourX/documentation.pm
@@ -4,11 +4,14 @@ use Dancer ":syntax";
 use common::sense;
 
 
-
 # Lists all routes the currently authenticated user has access to
 # Of course they may not be authorised to access the resource at that route - but they can try
 get "/documentation/route_list" => sub {
-    return Harbour::route_list();
+    my @route_list = values Harbour::route_list();
+
+    return {
+        routes => \@route_list
+    };
 };
 
 
